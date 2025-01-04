c81c6c9d0efd3ad53e404bd406ae7ffce5d82eee
loocusttamil <loocusttamil@gmail.com>
2025-01-04 21:42:52 +0530

    &&

--- relay_control/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import control_relay, get_relay_status, relay_control_page
-
-urlpatterns = [
-    path('control_relay/', control_relay, name='control_relay'),
-    path('get_relay_status/', get_relay_status, name='get_relay_status'),
-    path('', relay_control_page, name='relay_control_page'),
-]
